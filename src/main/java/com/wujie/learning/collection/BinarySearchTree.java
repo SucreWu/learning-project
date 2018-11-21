beeb6001265f9cc699d7632386eb487d67aab99a
wujie <wujie.0504@cashbus.com>
2018-11-21 19:35:09 +0800

    重构二叉树

--- src/main/java/com/wujie/learning/collection/BinarySearchTree.java
+++ src/main/java/com/wujie/learning/collection/BinarySearchTree.java
@@ -9,27 +9,94 @@ import java.util.*;
  **/
 public class BinarySearchTree<T extends Comparable<T>> {
 
+    // 内部结点类
+    class Node<T>{
+        public Node<T> left;
+        public Node<T> right;
+        public Node<T> parent;
+        public T data;
+        // 标记当前结点已经访问到哪一步。每次从子结点回溯回来的时候，都可以直接从当前结点里取出（用于非递归遍历的第二种实现）
+        public int state;
+
+        public Node(T data){
+            this.data = data;
+        }
+    }
+
+    // 根结点
     Node<T> root ;
 
-    //是否包含
+    // 查找二叉树的最小结点
+    private Node<T> minimum(Node<T> x){
+        if (x == null){
+            return null;
+        }
+        while (x.left != null){
+            x = x.left;
+        }
+        return x;
+    }
+
+    // 查找二叉树的最小值
+    private T getMinimum(){
+        Node<T> x = minimum(root);
+        if (x != null){
+            return x.data;
+        }
+        return null;
+    }
+
+    // 查找二叉树的最大结点
+    private Node<T> maximum(Node<T> x){
+        if (x == null){
+            return null;
+        }
+        while (x.right != null){
+            x = x.right;
+        }
+        return x;
+    }
+
+    // 查找二叉树的最大值
+    private T getMaximum(){
+        Node<T> x = maximum(root);
+        if (x != null){
+            return x.data;
+        }
+        return null;
+    }
+
+    // 获取后继结点，即二叉树中数据值大于该结点的最小结点
+    // 首先明确一点，既然是数值大于该结点，那么不可能在当前节点的左子树中寻找，所以不用考虑该结点左孩子的情况
+    private Node<T> getSuccessor(Node<T> x){
+        // 如果存在右孩子，则后继结点是当前结点右子树的最小结点
+        if (x.right != null){
+            return minimum(x);
+        }
+        // 如果没有右孩子，有以下两种情况：
+        // ①该结点是一个左孩子，那么x的后继结点就是他的父结点
+        // ②该结点是一个右孩子，查找x的最低父结点，且父结点必须具有左孩子，那么这个父结点就是x得后继结点
+        Node<T> parent = x.parent;
+//        while ((parent != null) && (parent.right == x))
+        while ((parent != null) && (parent.left == null)){
+            x = parent;
+            parent = parent.parent;
+        }
+        return parent;
+    }
+
+    // 是否存在
     private boolean contains(T t){
-        if(root == null || t == null){
+        if (root == null || t == null){
             return false;
         }
-        Node<T> x = root;
-        while(true){
-            if(t.compareTo(x.data) < 0){
-                if(x.left == null){
-                    break;
-                }else {
-                    x = x.left;
-                }
-            }else if(t.compareTo(x.data) > 0){
-                if(x.right == null){
-                    break;
-                }else {
-                    x = x.right;
-                }
+        Node<T> current = root;
+        int compare = t.compareTo(current.data);
+        while (current != null){
+            if (compare < 0) {
+                current = current.left;
+            }else if (compare > 0){
+                current = current.right;
             }else {
                 return true;
             }
@@ -37,152 +104,164 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return false;
     }
 
-    //插入
-    private boolean insert(T t){
-        if(t == null){
-            return false;
+    // 根据特定的值查找结点
+    private Node<T> find(T t){
+        if (root == null || t == null){
+            return null;
         }
-        if(root == null){
-            root = new Node<T>(null,t);
+        Node<T> current = root;
+        int compare = t.compareTo(current.data);
+        while (current != null){
+            if (compare < 0) {
+                current = current.left;
+            }else if (compare > 0){
+                current = current.right;
+            }else {
+                return current;
+            }
+        }
+        return null;
+    }
+
+    // 插入
+    private boolean insert(T t){
+        // 新结点
+        Node<T> newNode = new Node<>(t);
+        // 新结点的父结点
+        Node<T> parent = null;
+        // 二叉树为空
+        if (root == null){
+            root = newNode;
             return true;
         }
-
-        Node<T> x = root;
-        while(true){
-            if(t.compareTo(x.data) < 0){
-               if(x.left == null){
-                    x.left = new Node<T>(x, t);
-                    x.left.parent = x;
-                    break;
-               }else {
-                    x = x.left;
-               }
-            }else if(t.compareTo(x.data) > 0) {
-                if(x.right == null){
-                    x.right = new Node<T>(x,t);
-                    x.right.parent = x;
-                    break;
-                }else {
-                    x = x.right;
-                }
+        // 获取插入结点的位置
+        Node<T> current = root;
+        int compare = t.compareTo(current.data);
+        while (current != null){
+            parent = current;
+            if (compare < 0){
+                current = current.left;
+            }else if (compare > 0) {
+                current = current.right;
             }else {
-                System.out.println("数据已存在：" + t);
                 return false;
             }
+        }
+        current = newNode;
+        current.parent = parent;
+        // 判断新结点是父结点的左/右孩子
+        if (current.data.compareTo(parent.data) < 0){
+            parent.left = current;
+        }else {
+            parent.right = current;
         }
         return true;
     }
 
-    //删除
-    private void remove(Node<T> x){
-        Node<T> next ;
+    // 删除，分三种情况，第一：左右孩子都为空的结点就直接删了，第二：左右都不为空，寻找二叉树中数据值大于该结点的最小结点（后继结点）
+    // 第三：只有一个孩子结点，直接用孩子结点替换
+    private boolean remove(Node<T> x){
+        // 替代结点，用于第二种情况
+        Node<T> alternative;
+        // 左或者右孩子结点
         Node<T> child;
-        Node<T> p = x.parent;
-
-        //直接删除叶子结点
-        if(x.left == null && x.right == null){
-            //跟结点
-            if(p == null){
-                root = null;
-                return;
-            }
-            //分左右两种情况
-            if(p.left == x){
-                p.left = null;
-                return;
-            }else if (p.right == x){
-                p.right = null;
-                return;
-            }
-        }else if (x.left != null && x.right != null){ //中间结点就寻找中序后继，再递归调用
-            next = successor(x);
-            x.data = next.data;
-            remove(next);
+        Node<T> parent = x.parent;
+        // 第二种情况
+        if(x.left != null && x.right != null){
+            alternative = getSuccessor(x);
+            x.data = alternative.data;
+            // 把当前结点设置为后继结点，便于下面的删除
+            x = alternative;
+        }
+        // 因为如果是第二种情况，经过处理，后继结点只能是有一个孩子结点或者没有孩子结点
+        // 获取子结点
+        if (x.left != null){
+            child = x.left;
         }else {
-            if(x.left == null){
-                child = x.right;
-            }else {
-                child = x.left;
-            }
-            //跟结点
-            if(x == root){
-                root = child;
-                child.parent = null;
-                return;
-            }
-
-            if(x == p.left){
-                p.left = child;
-                child.parent = p;
-            }else {
-                p.right = child;
-                child.parent = p;
-            }
+            child = x.right;
         }
-
-
+        // 第三种情况，只有一个子结点
+        if (child != null){
+            // 将子结点关联父结点
+            child.parent = x.parent;
+        }
+        // 删除的结点是根结点
+        // 第二种没有子结点的情况，也会在这里被删除（将父结点的左/右孩子置为null）
+        if (x.parent == null){
+            root = child;
+        }else if(x == x.parent.left){
+            x.parent.left = child;
+        }else {
+            x.parent.right = child;
+        }
+//        比较通俗易懂的版本
+//        if (x.left == null && x.right == null) {
+//            // 根结点
+//            if (parent == null) {
+//                root = null;
+//            }
+//            if (parent.left == x) {
+//                parent.left = null;
+//            } else {
+//                parent.right = null;
+//            }
+//        }else if(x.left != null && x.right != null){
+//            alternative = getSuccessor(x);
+//            x.data = alternative.data;
+//            remove(alternative); // 递归删除
+//        }else{
+//            if (x.left != null){
+//                child = x.left;
+//            }else {
+//                child = x.right;
+//            }
+//            child.parent = parent;
+//            // 根结点
+//            if (parent == null){
+//                root = child;
+//                child.parent = null;
+//            }
+//            // 分删除结点是父结点的左右子结点两种情况
+//            if (parent.left == x){
+//                parent.left = child;
+//            }else {
+//                parent.right = child;
+//            }
+//        }
+        return true;
     }
 
-    //传进来的当前节点作为根节点，因为是中序，所以下一个先查找右子树，再继续按照左中右的顺序循环查找左子树
-    //如果当前节点不存在右子树，先查找父节点，直到找到该节点是在某个祖先节点的左子树上
-    //说白了就是在中序遍历的时候寻找下一个结点
-    public Node<T> successor(Node<T> x) {
-        if (x == null)
-            return null;
-        Node<T> p;
-        if (x.right != null) {
-            p = x.right;
-            while (p.left != null) {
-                p = p.left;
-            }
-            return p;
-        }
-        else {
-            p = x.parent;
-            while (p != null && p.left != x) {
-                x = p;
-                p = x.parent;
-            }
-            return p;
-        }
-    }
-
-    //前序遍历（根 左 右）
+    // 递归实现
+    // 前序遍历（根 左 右）
     private void preOrder(Node<T> x){
-        System.out.println(x.data);
-        if(x.left != null){
+        if (x.left != null){
+            System.out.println(x.data);
             preOrder(x.left);
-        }
-        if(x.right != null){
             preOrder(x.right);
         }
     }
 
-    //中序遍历（左 根 右）
+    // 中序遍历（左 根 右）
     private void inOrder(Node<T> x){
-        if(x.left != null){
+        if (x.left != null){
             inOrder(x.left);
-        }
-        System.out.println(x.data);
-        if(x.right != null){
+            System.out.println(x.data);
             inOrder(x.right);
         }
     }
 
-    //后序遍历（左 右 根）
-    private void postOrder(Node<T> x){
-        if(x.left != null){
+    // 后序遍历（左 右 根）
+    private void postOrder(Node<T> x) {
+        if (x.left != null) {
             postOrder(x.left);
-        }
-        if(x.right != null){
             postOrder(x.right);
+            System.out.println(x.data);
         }
-        System.out.println(x.data);
     }
 
     //非递归前序遍历
-    public void preOrderWithoutRecurs(){
-        if (root == null){
+    public void preOrderWithoutRecurs() {
+        if (root == null) {
             return;
         }
 
@@ -216,7 +295,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
         if (root == null){
             return;
         }
-
         MyStack<Node<T>> stack = new MyStack<>(50);
         Node<T> current;
         stack.push(root);
@@ -247,7 +325,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
         if (root == null){
             return;
         }
-
         MyStack<Node<T>> stack = new MyStack<>(50);
         Node<T> current;
         stack.push(root);
@@ -293,30 +370,4 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
-    private static class Node<T>{
-        Node<T> left;
-        Node<T> right;
-        Node<T> parent; //便于查找中序后继
-        T data;
-        int state;
-
-        Node(Node<T> parent, T data){
-            this.left = null;
-            this.right = null;
-            this.data = data;
-            this.parent = parent;
-        }
-    }
-
-    public static void main(String[] args) {
-        BinarySearchTree bTree = new BinarySearchTree();
-        bTree.insert(4);
-        bTree.insert(2);
-        bTree.insert(1);
-        bTree.insert(3);
-        bTree.insert(5);
-        bTree.inOrderWithoutRecurs();
-        bTree.remove(bTree.root.left);
-        bTree.inOrderWithoutRecurs();
-    }
 }
