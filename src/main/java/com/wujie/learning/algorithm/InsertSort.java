3d8dc284dd9521444da72531663a876722ebf345
wujie <wujie.0504@cashbus.com>
2018-12-18 11:17:15 +0800

    插入排序

---
+++ src/main/java/com/wujie/learning/algorithm/InsertSort.java
@@ -0,0 +1,33 @@
+package com.wujie.learning.algorithm;
+
+/**
+ * @Description: 插入排序
+ * @Auther: wujie
+ * @Date: 2018/12/17 18:10
+ * @version: V1.0
+ */
+public class InsertSort {
+    static void insertSort(int[] array){
+        // 从1开始是因为数组的第一个元素被当做初始的有序数组
+        for (int i = 1; i < array.length; i++){
+            // 待插入数值
+            int temp = array[i];
+            int j = i-1;
+            // 满足条件则表示寻找到插入位置的下标
+            for (; j >= 0 && temp < array[j]; j--){
+                // 将当前下标的数向右移动一位，同时继续向左查找
+                array[j+1] = array[j];
+            }
+            // j+1在j子减后就是插入位置的下标
+            array[j+1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3,6,7,2,1,4,10,22,5};
+        insertSort(a);
+        for(int i : a){
+            System.out.println(i);
+        }
+    }
+}
